38224d1bd94ea3f2c607aef26239cce9ddd6b5ee
OrangeLanternGames <orangelanterngames@gmail.com>
2026-06-01 09:14:33 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<div style="padding:2rem;font-family:monospace">DIAL IN</div>`,
+})
+export class AppComponent {}
